719d7ef6d70765e7095d59334fe6ca9bbf60dc59
mmurido <teresadee002@gmail.com>
2024-02-05 21:30:09 +0100

    Creazione della classe PgDatabaseConnector
    Sono state inserite la libreria HikariCP e le sue dipendenze per poter creare una connection pool

---
+++ unina.social.group/src/main/java/util/PgDatabaseConnector.java
@@ -0,0 +1,54 @@
+package util;
+
+import java.sql.*;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+public class PgDatabaseConnector {
+	
+    private static final HikariDataSource dataSource;
+
+    static {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:postgresql://localhost:5432/UninaSocialGroupDB");
+        config.setUsername("postgres");
+        config.setPassword("!Teresa!002!");
+        config.setMaximumPoolSize(5);
+        dataSource = new HikariDataSource(config);
+    }
+
+    public static Connection getConnection() throws SQLException {
+    	try 
+    	{
+            return dataSource.getConnection();
+    	} 
+    	catch(SQLException e) 
+    	{
+    		System.out.println("Connessione alla base di dati fallita." + e);
+    		System.out.println(e);
+        	return null;
+    	}
+    }
+    
+    public void closeResources(Statement stmt, ResultSet rs, Connection con) {
+    	try 
+    	{
+    		if (stmt != null && !stmt.isClosed()) {
+    			stmt.close();
+    		}
+    		
+    		if (rs != null && !rs.isClosed()) {
+    			rs.close();
+    		}
+    		
+    		if (con != null && !con.isClosed()) {
+    			con.close();
+    		}
+    	} 
+    	catch (SQLException e) 
+    	{
+    		System.out.println(e);
+    	}
+    }
+
+}
